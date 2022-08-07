512df8e5827dd4394e82861146a2f04f4849e635
FuturumTech <109477170+FuturumTech@users.noreply.github.com>
2022-08-07 17:23:16 +0100

    Add files via upload
    Created mock database for grpc RoomStatus

---
+++ src/main/java/ds/service1/Service1DataBase.java
@@ -0,0 +1,13 @@
+package ds.service1;
+
+import java.util.ArrayList;
+
+public class Service1DataBase {
+	ArrayList<roomName> myRooms = new ArrayList<>();
+	
+	public class roomName{
+		private String roomName;
+		private double currentHumidity, currentTempInCelcius;
+		
+	}
+}
