d277a82554502bc6a7b4be5e59c88f4f5ff6dd8a
FuturumTech <109477170+FuturumTech@users.noreply.github.com>
2022-08-10 23:35:31 +0100

    Add files via upload
    Service registration done for Service 2

--- src/main/java/ds/service1/Service1.java
+++ src/main/java/ds/service1/Service1.java
@@ -21,30 +21,31 @@ public class Service1 extends Service1ImplBase {
 
 	private static final Logger logger = Logger.getLogger(Service1.class.getName());
 	private static Service1DataBase myTempData = new Service1DataBase();
+
 	// 1. Create and start the server
 	public static void main(String[] args) throws InterruptedException, IOException {
-				//Adding mock Room details to database Service 1:
-				Service1DataBase myTempData = new Service1DataBase();
+		// Adding mock Room details to database Service 1:
+		Service1DataBase myTempData = new Service1DataBase();
 
-				Service1DataBase.RoomName temp1 = new Service1DataBase.RoomName("consultation room", 73, 22.5);
-				Service1DataBase.RoomName temp2 = new Service1DataBase.RoomName("hallway", 65, 24.5);
-				Service1DataBase.RoomName temp3 = new Service1DataBase.RoomName("reception", 49, 18.4);
-				Service1DataBase.RoomName temp4 = new Service1DataBase.RoomName("canteen", 89, 26.5);
-				myTempData.getMyRooms().add(temp1);
-				myTempData.getMyRooms().add(temp2);
-				myTempData.getMyRooms().add(temp3);
-				myTempData.getMyRooms().add(temp4);
-				//testing if added properly
-				System.out.println("Room index 2 is: " + myTempData.getMyRooms().get(2).getRoomName());
-				System.out.println("ArrayList size is: " + myTempData.getMyRooms().size());
-				
-			// Create and Instance of the Class to five access to the RPC methods/services
-			Service1 service1 = new Service1();
-			Properties prop = service1.getProperties();
-			service1.registerService(prop);
-			
-			// Port where the serves is located
-			int port = Integer.valueOf( prop.getProperty("service_port") );// #.50051;
+		Service1DataBase.RoomName temp1 = new Service1DataBase.RoomName("consultation room", 73, 22.5);
+		Service1DataBase.RoomName temp2 = new Service1DataBase.RoomName("hallway", 65, 24.5);
+		Service1DataBase.RoomName temp3 = new Service1DataBase.RoomName("reception", 49, 18.4);
+		Service1DataBase.RoomName temp4 = new Service1DataBase.RoomName("canteen", 89, 26.5);
+		myTempData.getMyRooms().add(temp1);
+		myTempData.getMyRooms().add(temp2);
+		myTempData.getMyRooms().add(temp3);
+		myTempData.getMyRooms().add(temp4);
+		// testing if added properly
+		System.out.println("Room index 2 is: " + myTempData.getMyRooms().get(2).getRoomName());
+		System.out.println("ArrayList size is: " + myTempData.getMyRooms().size());
+
+		// Create and Instance of the Class to five access to the RPC methods/services
+		Service1 service1 = new Service1();
+		Properties prop = service1.getProperties();
+		service1.registerService(prop);
+
+		// Port where the serves is located
+		int port = Integer.valueOf(prop.getProperty("service_port"));// #.50051;
 		try {
 			// Adding the service via the class instance and starting the server here.
 			Server server = ServerBuilder.forPort(port).addService(service1).build().start();
@@ -113,7 +114,7 @@ public class Service1 extends Service1ImplBase {
 			Thread.sleep(1000);
 
 			// Unregister all services
-			// jmdns.unregisterAllServices();
+			jmdns.unregisterAllServices();
 
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
@@ -128,37 +129,38 @@ public class Service1 extends Service1ImplBase {
 	public void desiredSettingHVAC(DesiredRoomConditions request, StreamObserver<Confirmation> responseObserver) {
 		// Find out what sent by the client
 		String roomName = request.getRoomName();
-		String roomNameToReturn= request.getRoomName();
+		String roomNameToReturn = request.getRoomName();
 		double desiredHumidity = request.getDesiredHumidity();
 		double desiredTempInCelcius = request.getDesiredTempInCelcius();
-		System.out.println("On Server side (desiredSettingHVAC) received: \n\t" + roomName + " (room name) \n\t" + desiredHumidity + "(desired humidity) \n\t" + desiredTempInCelcius + "(desired temperature)");
-		try{
+		System.out.println("On Server side (desiredSettingHVAC) received: \n\t" + roomName + " (room name) \n\t"
+				+ desiredHumidity + "(desired humidity) \n\t" + desiredTempInCelcius + "(desired temperature)");
+		try {
 			Service1DataBase.RoomName roomInput = findRoom(roomName);
-			//if the room name exist in the database:
-			if(roomInput != null) {
+			// if the room name exist in the database:
+			if (roomInput != null) {
 				roomInput.setDesiredHumidity(desiredHumidity);
 				roomInput.setDesiredTempInCelcius(desiredTempInCelcius);
 				roomName = roomInput.getRoomName();
-			}else {
+			} else {
 				roomName = "not found";
 			}
-		}catch(NullPointerException e) {
+		} catch (NullPointerException e) {
 			e.getMessage();
 		}
-	
 
 		// Our response:
 		// Firstly, we must create a builder
 
-		//case if the room name does not exist (will be overwritten if the room exists
-		Confirmation.Builder responseBuilder= Confirmation.newBuilder()
+		// case if the room name does not exist (will be overwritten if the room exists
+		Confirmation.Builder responseBuilder = Confirmation.newBuilder()
 				.setConfirmation("Receiving stream of data to grp hVACstatus completed");
-		//Case if the room name exist
-		if(!roomName.equals("not found")) {
-			 responseBuilder = Confirmation.newBuilder()
-					.setConfirmation("Receiving stream of data to grp hVACstatus was completed for room: \"" +roomNameToReturn+ "\" was compelted"); 
-		}else {
-			responseBuilder= Confirmation.newBuilder()
+		// Case if the room name exist
+		if (!roomName.equals("not found")) {
+			responseBuilder = Confirmation.newBuilder()
+					.setConfirmation("Receiving stream of data to grp hVACstatus was completed for room: \""
+							+ roomNameToReturn + "\" was compelted");
+		} else {
+			responseBuilder = Confirmation.newBuilder()
 					.setConfirmation("No room was found for the name:" + roomNameToReturn);
 		}
 		// Send it back:
@@ -175,6 +177,7 @@ public class Service1 extends Service1ImplBase {
 		return new StreamObserver<CurrentRoomConditions>() {
 			String roomName;
 			String roomNameToReturn;
+
 			@Override
 			public void onNext(CurrentRoomConditions value) {
 				// TODO Auto-generated method stub
@@ -182,18 +185,19 @@ public class Service1 extends Service1ImplBase {
 				roomNameToReturn = value.getRoomName();
 				double humidity = value.getHumidity();
 				double tempInCelcius = value.getTempInCelcius();
-				System.out.println("On Server side (hVACstatus) received: \n\t" + roomName + " (room name) \n\t"+ humidity + "(humidity) \n\t" + tempInCelcius + "(temperature)");
-				try{
+				System.out.println("On Server side (hVACstatus) received: \n\t" + roomName + " (room name) \n\t"
+						+ humidity + "(humidity) \n\t" + tempInCelcius + "(temperature)");
+				try {
 					Service1DataBase.RoomName roomInput = findRoom(roomName);
-					//if the room name exist in the database:
-					if(roomInput != null) {
+					// if the room name exist in the database:
+					if (roomInput != null) {
 						roomInput.setCurrentHumidity(humidity);
 						roomInput.setCurrentTempInCelcius(tempInCelcius);
 						roomName = roomInput.getRoomName();
-					}else {
+					} else {
 						roomName = "not found";
 					}
-				}catch(NullPointerException e) {
+				} catch (NullPointerException e) {
 					e.getMessage();
 				}
 			}
@@ -209,18 +213,18 @@ public class Service1 extends Service1ImplBase {
 				// TODO Auto-generated method stub
 				// Our response:
 				// Firstly, we must create a builder
-				//case if the room name does not exist (will be overwritten if the room exists
-				Acknowledged.Builder responseBuilder= Acknowledged.newBuilder()
+				// case if the room name does not exist (will be overwritten if the room exists
+				Acknowledged.Builder responseBuilder = Acknowledged.newBuilder()
 						.setAcknowledgment("Receiving stream of data to grp hVACstatus completed");
-				//Case if the room name exist
-				if(!roomName.equals("not found")) {
-					 responseBuilder = Acknowledged.newBuilder()
-							.setAcknowledgment("Receiving stream of data to grp hVACstatus was completed for room: \"" +roomNameToReturn+ "\" was compelted"); 
-				}else {
-					responseBuilder= Acknowledged.newBuilder()
+				// Case if the room name exist
+				if (!roomName.equals("not found")) {
+					responseBuilder = Acknowledged.newBuilder()
+							.setAcknowledgment("Receiving stream of data to grp hVACstatus was completed for room: \""
+									+ roomNameToReturn + "\" was compelted");
+				} else {
+					responseBuilder = Acknowledged.newBuilder()
 							.setAcknowledgment("No room was found for the name:" + roomNameToReturn);
 				}
-				
 
 				// Send it back:
 				responseObserver.onNext(responseBuilder.build());
@@ -232,44 +236,41 @@ public class Service1 extends Service1ImplBase {
 
 	@Override
 	public void roomStatus(Room request, StreamObserver<AdjustHVAC> responseObserver) {
-		
+
 		// Find out what sent by the client
 
 		AdjustHVAC responseBuilder;
 		double[] tempArray = new double[2];
 		String roomName = request.getRoomName();
-		
-		try{
+
+		try {
 			Service1DataBase.RoomName roomInput = findRoom(roomName);
-			
-			//if the room name exist in the database:
-			if(roomInput != null) {
-			//Alternative if statement:
-			//if(myTempData.getMyRooms().contains(roomName.trim().toLowerCase())) {
+
+			// if the room name exist in the database:
+			if (roomInput != null) {
+				// Alternative if statement:
+				// if(myTempData.getMyRooms().contains(roomName.trim().toLowerCase())) {
 				double roomHumidity = roomInput.getCurrentHumidity();
 				double roomTemperature = roomInput.getCurrentTempInCelcius();
-				
+
 				tempArray = calculateDifference(roomInput);
-				
-				
-				
+
 				// Our response:
 				// Firstly, we must create a builder
 				responseBuilder = AdjustHVAC.newBuilder().setHumidityDifference(tempArray[0])
 						.setTempDifference(tempArray[1]).build(); // PLACEHOLDER FOR VALUE
-				
-			}else {
-				//-999 App will read it that the input was incorrect 
-				responseBuilder = AdjustHVAC.newBuilder().setHumidityDifference(-999)
-						.setTempDifference(-999).build();
+
+			} else {
+				// -999 App will read it that the input was incorrect
+				responseBuilder = AdjustHVAC.newBuilder().setHumidityDifference(-999).setTempDifference(-999).build();
 			}
 
 			// // RETURN
 			// Send it back:
-			//Server streaming:
-			for(double d: tempArray) {
+			// Server streaming:
+			for (double d : tempArray) {
 				responseObserver.onNext(responseBuilder);
-				
+
 				// Wait a bit
 				try {
 					Thread.sleep(5000);
@@ -278,38 +279,39 @@ public class Service1 extends Service1ImplBase {
 					e.printStackTrace();
 				}
 			}
-			
-		}catch(NullPointerException e){
+
+		} catch (NullPointerException e) {
 			e.getMessage();
 		}
-		
-		
+
 		responseObserver.onCompleted();
 
-
 	}
-	//method to find room name in ArrayList for grpc roomStatus
-		 private static Service1DataBase.RoomName findRoom(String room) {
-			 //System.out.println("ArrayList size is in findRoom method is: " + Service1.myTempData.getMyRooms().size());
-			 for(int i=0; i<Service1.myTempData.getMyRooms().size(); i++) {
-		    //for(Service1DataBase.roomName roomName : myTempData.getMyRooms()) {
-		        if(Service1.myTempData.getMyRooms().get(i).getRoomName().equalsIgnoreCase(room)) {
-		            return Service1.myTempData.getMyRooms().get(i);
-		        }
-		    }
-		    return null;
+
+	// method to find room name in ArrayList for grpc roomStatus
+	private static Service1DataBase.RoomName findRoom(String room) {
+		// System.out.println("ArrayList size is in findRoom method is: " +
+		// Service1.myTempData.getMyRooms().size());
+		for (int i = 0; i < Service1.myTempData.getMyRooms().size(); i++) {
+			// for(Service1DataBase.roomName roomName : myTempData.getMyRooms()) {
+			if (Service1.myTempData.getMyRooms().get(i).getRoomName().equalsIgnoreCase(room)) {
+				return Service1.myTempData.getMyRooms().get(i);
+			}
 		}
-		//method to calculate difference in ArrayList for grpc roomStatus
-		 private static double[] calculateDifference(Service1DataBase.RoomName room) {
-			 double[] tempArray = new double[2];
-			 tempArray[0] = room.getCurrentHumidity() - room.getDesiredHumidity();
-			 tempArray[1] = room.getCurrentTempInCelcius() - room.getDesiredTempInCelcius();
-			//rounding to 1 decimal:
-			 tempArray[0] = (Math.round(tempArray[0]*10.0))/10.0;
-			 tempArray[1] = (Math.round(tempArray[1]*10.0))/10.0;
-			 return tempArray;
-		 }
-	
+		return null;
+	}
+
+	// method to calculate difference in ArrayList for grpc roomStatus
+	private static double[] calculateDifference(Service1DataBase.RoomName room) {
+		double[] tempArray = new double[2];
+		tempArray[0] = room.getCurrentHumidity() - room.getDesiredHumidity();
+		tempArray[1] = room.getCurrentTempInCelcius() - room.getDesiredTempInCelcius();
+		// rounding to 1 decimal:
+		tempArray[0] = (Math.round(tempArray[0] * 10.0)) / 10.0;
+		tempArray[1] = (Math.round(tempArray[1] * 10.0)) / 10.0;
+		return tempArray;
+	}
+
 //	//Extend abstract base class for our implementation
 //	static class NewServerImpl extends Service1ImplBase{
 //		@Override
--- src/main/java/ds/service2/Service2.java
+++ src/main/java/ds/service2/Service2.java
@@ -1,8 +1,15 @@
 package ds.service2;
 
+import java.io.FileInputStream;
 import java.io.IOException;
-
+import java.io.InputStream;
+import java.net.InetAddress;
 import java.util.ArrayList;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceInfo;
 
 import ds.service1.Service1;
 import ds.service1.Service1DataBase;
@@ -13,7 +20,7 @@ import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
 
 public class Service2 extends Service2ImplBase {
-
+	private static final Logger logger = Logger.getLogger(Service2.class.getName());
 	private static Service2DataBase myTempData = new Service2DataBase();
 
 	public static void main(String[] args) throws InterruptedException, IOException {
@@ -41,15 +48,29 @@ public class Service2 extends Service2ImplBase {
 		// myTempData.getDesk().get(2).getRoomName());
 		// System.out.println("ArrayList size is: " + myTempData.getDesk().size());
 
-		Service2 service1 = new Service2();
+		// Create and Instance of the Class to five access to the RPC methods/services
+		Service2 service2 = new Service2();
+		Properties prop = service2.getProperties();
+		service2.registerService(prop);
 
-		int port = 50052;
+		// Port where the serves is located
+		int port = Integer.valueOf(prop.getProperty("service_port"));// #.50052;
+		// int port = 50052;
 
-		Server server = ServerBuilder.forPort(port).addService(service1).build().start();
+		try {
+			// Adding the service via the class instance and starting the server here.
+			Server server = ServerBuilder.forPort(port).addService(service2).build().start();
 
-		System.out.println("Service-2 started, listening on " + port);
-
-		server.awaitTermination();
+			logger.info("Server started, listening on " + port);
+			// Server is waiting until explicitly terminated
+			server.awaitTermination();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -69,33 +90,41 @@ public class Service2 extends Service2ImplBase {
 		boolean isHeightAdjusted = false;
 		DeskAdjustedResponse reply;
 		try {
-			//result of the below method may be null if not found, therefore try catch
+			// result of the below method may be null if not found, therefore try catch
 			Service2DataBase.Desk myDesk = findDesk(request.getRoomName(), request.getDeskNumber());
-			//if client request is for increment of desk height
+			// if client request is for increment of desk height
 			if (myDesk != null && request.getOperation() == DeskDetailsRequest.Operation.DESKUP) {
-				//request is an increment of desiredDeskHeiht
+				// request is an increment of desiredDeskHeiht
 				int newHeightIncrement = myDesk.getDeskHeight() + deskHeightChangeRequest;
-				//below method validate range of the height and throws custom exception:
+				// below method validate range of the height and throws custom exception:
 				myDesk.setDeskHeight(newHeightIncrement);
 				isHeightAdjusted = true;
+				System.out.println("deskStatusHeight() method run correctly, new desk height is: "
+						+ myDesk.getDeskHeight() + " increased by: " + deskHeightChangeRequest);
 			}
-			//else if client request is for decrement of desk height
+			// else if client request is for decrement of desk height
 			else if (myDesk != null && request.getOperation() == DeskDetailsRequest.Operation.DESKDOWN) {
-				//request is an increment of desiredDeskHeiht
+				// request is an increment of desiredDeskHeiht
 				int newHeightDecrement = myDesk.getDeskHeight() - deskHeightChangeRequest;
-				//below method validate range of the height and throws custom exception:
+				// below method validate range of the height and throws custom exception:
 				myDesk.setDeskHeight(newHeightDecrement);
 				isHeightAdjusted = true;
+				System.out.println("deskStatusHeight() method run correctly, new desk height is: "
+						+ myDesk.getDeskHeight() + " decreased by: " + deskHeightChangeRequest);
 			}
-			// preparing the response message, case if the desk is found and adjusted correctly
-			reply = DeskAdjustedResponse.newBuilder().setDeskHeight(myDesk.getDeskHeight()).setIsHeightAdjusted(isHeightAdjusted).build();
+			// preparing the response message, case if the desk is found and adjusted
+			// correctly
+			reply = DeskAdjustedResponse.newBuilder().setDeskHeight(myDesk.getDeskHeight())
+					.setIsHeightAdjusted(isHeightAdjusted).build();
 		} catch (NullPointerException ex) {
-			ex.getMessage();
-			// preparing the response message, -999 means to the client that unusual path reached and boolean isHeightAdjusted will remain false
+			System.out.println(ex.getMessage());
+			// preparing the response message, -999 means to the client that unusual path
+			// reached and boolean isHeightAdjusted will remain false
 			reply = DeskAdjustedResponse.newBuilder().setDeskHeight(-999).setIsHeightAdjusted(isHeightAdjusted).build();
-		} catch(IllegalDeskHeightException e) {
-			e.getMessage();
-			// preparing the response message, -999 means to the client that unusual path reached and boolean isHeightAdjusted will remain false
+		} catch (IllegalDeskHeightException e) {
+			System.out.println(e.getMessage());
+			// preparing the response message, -999 means to the client that unusual path
+			// reached and boolean isHeightAdjusted will remain false
 			reply = DeskAdjustedResponse.newBuilder().setDeskHeight(-999).setIsHeightAdjusted(isHeightAdjusted).build();
 		}
 
@@ -121,4 +150,66 @@ public class Service2 extends Service2ImplBase {
 		return null;
 	}
 
+	// Service registration:
+	// 1st -Method to get properties of the service:
+	private Properties getProperties() {
+
+		Properties prop = null;
+
+		try (InputStream input = new FileInputStream("src/main/resources/service2.properties")) {
+
+			prop = new Properties();
+
+			// load a properties file
+			prop.load(input);
+
+			// get the property value and print it out
+			System.out.println("Service1 Climate Control properies ...");
+			System.out.println("\t service_type: " + prop.getProperty("service_type"));
+			System.out.println("\t service_name: " + prop.getProperty("service_name"));
+			System.out.println("\t service_description: " + prop.getProperty("service_description"));
+			System.out.println("\t service_port: " + prop.getProperty("service_port"));
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
+		return prop;
+	}
+
+	// 2nd is to register the service
+	private void registerService(Properties prop) {
+
+		try {
+			// Create a JmDNS instance
+			JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+
+			String service_type = prop.getProperty("service_type");// "_service2deskusage._tcp.local.";
+			String service_name = prop.getProperty("service_name");// "desk_usage";
+			// int service_port = 1234;
+			int service_port = Integer.valueOf(prop.getProperty("service_port"));// #.50052;
+
+			String service_description_properties = prop.getProperty("service_description");// "path=index.html";
+
+			// Register a service
+			ServiceInfo serviceInfo = ServiceInfo.create(service_type, service_name, service_port,
+					service_description_properties);
+			jmdns.registerService(serviceInfo);
+
+			System.out.printf("registrering service with type %s and name %s \n", service_type, service_name);
+
+			// Wait a bit
+			Thread.sleep(1000);
+
+			// Unregister all services
+			jmdns.unregisterAllServices();
+
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
